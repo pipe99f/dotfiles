47566be5d4321e32256fab09330fd4a9cbeee6d9
pipe99f <felipe99al@gmail.com>
2026-06-10 19:20:28 -0500

    fix: remove solution for legacy bug in nvim-ts-autotag

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -658,28 +658,20 @@ return {
 	{
 		"windwp/nvim-ts-autotag",
 		ft = { "html", "typescript", "javascript", "markdown" },
-		config = function()
-			require("nvim-ts-autotag").setup()
-			vim.lsp.handlers["textDocument/publishDiagnostics"] =
-				-- vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-				-- 	underline = true,
-				-- 	virtual_text = {
-				-- 		spacing = 5,
-				-- 		severity_limit = "Warning",
-				-- 	},
-				-- 	update_in_insert = true,
-				-- })
-				vim.diagnostic.config({
-					underline = true,
-					virtual_text = {
-						spacing = 5,
-						-- In the new API, 'severity_limit' is replaced by 'severity'
-						-- This shows Warnings, Errors, and anything "above" Info
-						severity = { min = vim.diagnostic.severity.WARN },
-					},
-					update_in_insert = true,
-				})
-		end,
+		opts = {
+			-- Defaults
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			enable_close_on_slash = false, -- Auto close on trailing </
+		},
+		-- Also override individual filetype configs, these take priority.
+		-- Empty by default, useful if one of the "opts" global settings
+		-- doesn't work well in a specific filetype
+		-- per_filetype = {
+		-- 	["html"] = {
+		-- 		enable_close = false,
+		-- 	},
+		-- },
 	},
 	{
 		"Wansmer/treesj",
