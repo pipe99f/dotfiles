5f874d0a0f448f7462b660fa993f69bd1e105ca0
felipefzto <felipe.algarra@finanzauto.com.co>
2026-06-11 12:40:47 -0500

    remove gitgraph and add lazydocker

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -25,6 +25,25 @@ return {
 			end,
 		})
 
+		-- Lazydocker
+		local lazydocker = Terminal:new({
+			cmd = "lazydocker",
+			hidden = true,
+			direction = "float",
+			on_open = function(term)
+				vim.cmd("startinsert!")
+				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+			end,
+			float_opts = {
+				border = "double",
+			},
+		})
+		function lazy_docker_toggle()
+			lazydocker:toggle()
+		end
+		vim.keymap.set("n", "<M-d>", "<cmd>lua lazy_docker_toggle()<CR>", { desc = "Toggle lazydocker" })
+		vim.keymap.set("t", "<M-d>", "<C-\\><C-n><cmd>lua lazy_docker_toggle()<CR>", { desc = "Toggle lazydocker" })
+
 		local hiddenFloat = Terminal:new({
 			direction = "float",
 			count = 6,
@@ -39,3 +58,4 @@ return {
 		end, {})
 	end,
 }
+
